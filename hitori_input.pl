61f6aedf427b1db87c99456007656e537bfaaa24
Mbaez97 <marcelobaezparaguay@gmail.com>
2021-07-21 21:55:32 -0400

    Hitori_input mejorado

--- hitori_input.pl
+++ hitori_input.pl
@@ -1,12 +1,12 @@
 :- include('solver').
 
 % Runner
-run :- pistaHitori(X), see(X), tell(F), solveProblems(5), told, seen,!.
-run :- told, seen.
+run():- pistaHitori(X), solveProblems(5), told, seen,!.
+run():- told, seen.
 
 solveProblems(0).
-solveProblems(N):- 
-        N > 0, 
+solveProblems(N):-
+        N > 0,
         solveMatriz(X,Y,I,S), !,
         N1 is N-1,
         solveProblems(N1).
